5d0a57af3c6a1417b9450222cc5ee95d16a3f21c
wongRegina <wong.regina.1@gmail.com>
2020-03-11 15:51:55 -0400

    fixed throughput

--- PA2ptA.py
+++ PA2ptA.py
@@ -4,6 +4,7 @@ import dpkt
 
 class Packet:
     def __init__(self, timestamp, buffer):
+        self.buffer = buffer
         self.timestamp = timestamp
         # First 28 bytes are Ethernet II - buffer[0,14]
         self.Ethernet = buffer[0:14]
@@ -21,8 +22,9 @@ class Packet:
         self.destPort = self.TCP[2:4]
         self.seqNum = self.TCP[4:8]
         self.ackNum = self.TCP[8:12]
+        self.header_length = self.TCP[12] >> 2
         self.flags = self.TCP[12:14]
-        a = (self.flags)[1]
+        a = self.flags[1]
         # From flag get ack, syn, fin, push
         self.ack = (0b10000 & int(a)) >> 4
         self.push = (0b1000 & int(a)) >> 3
@@ -30,6 +32,8 @@ class Packet:
         self.fin = 0b1 & int(a)
         self.windSizeValue = self.TCP[14:16]
         self.checksum = self.TCP[16:18]
+        self.receiver_window_size = -1
+        self.payload = self.buffer[66:len(buffer)]
 
     def __str__(self):
         return "Source IP: %s, Destination IP: %s, Source Port: %s, " "Destination Port: " \
@@ -42,14 +46,14 @@ class Packet:
     def print(self):  # print sequence, ack num, window size for each flow in lists_of_flows
         return "Sequence number: %s, Acknowledgement Number: %s, Receive Window Size: %s" % \
                (str(int.from_bytes(self.seqNum, "big")), str(int.from_bytes(self.ackNum, "big")),
-                str(int.from_bytes(self.windSizeValue, "big")))
+                str((int.from_bytes(self.windSizeValue, "big") << self.receiver_window_size)))
 
 
 def find_packets(pcap):  # Goes read each element in the pcap file
     packets = []
     for timestamp, buffer in pcap:
         IP = buffer[14:34]
-        if int.from_bytes(IP[12:16],"big") == 0x82f5910c:
+        if int.from_bytes(IP[12:16], "big") == 0x82f5910c:
             packet = Packet(timestamp, buffer)
             packets.append(packet)
     return packets
@@ -67,14 +71,18 @@ def find_TCP_connections(packets):
 
 def find_flows(packets):
     list_of_flows = []
+    receiver_window_size = -1
     for packet in packets:
         if packet.ack != 1 and packet.syn == 1:
             a = [packet]
+            packet.receiver_window_size = int.from_bytes(packet.TCP[len(packet.TCP) - 1:len(packet.TCP)], "big")
+            receiver_window_size = packet.receiver_window_size
             list_of_flows.append(a)
         else:
             for flow in list_of_flows:
                 port = flow[0].sourcePort
                 if port == packet.destPort or port == packet.sourcePort:
+                    packet.receiver_window_size = receiver_window_size
                     flow.append(packet)
                     break
     return list_of_flows
@@ -83,13 +91,19 @@ def find_flows(packets):
 def going_through_flow(flow):
     sender = flow[0].sourceIP
     counter = 0
-    print("Port Number: ",  str(int.from_bytes(flow[0].sourcePort, "big")))
+    print("Port Number: ", str(int.from_bytes(flow[0].sourcePort, "big")))
+    total_time = flow[len(flow) - 1].timestamp - flow[0].timestamp
+    total_packets = 0
     for packet in flow:
-        if counter < 2:
-            if packet.syn == 0 and packet.ack == 1 and packet.push == 0 and packet.sourceIP == sender:
+        if packet.sourceIP == sender:
+
+            if packet.syn == 0 and packet.ack == 1 and packet.push == 0 and counter < 2:
                 counter += 1
                 print(packet.print())
-    print("\n")
+                total_packets += len(packet.payload) + packet.header_length
+            else:
+                total_packets += len(packet.payload) + packet.header_length
+    print("Sender Throughput: ", str(total_packets/total_time), " Bytes/sec\n")
 
 
 def main():
@@ -98,17 +112,8 @@ def main():
     packets = find_packets(pcap)
     list_of_flows = find_flows(packets)
     print("The number of TCP flows initiate from the sender", len(list_of_flows), "\n")
-    # part 2 of A: print sequence, ack num, window size for each flow in lists_of_flows
-    for flow in list_of_flows:
+    for flow in list_of_flows:  # part 2 of A: print sequence, ack num, window size for each flow in lists_of_flows
         going_through_flow(flow)
-    counter = 0
-    # for packet in packets:
-    #     print(packet)
-        # if counter < 50:
-        #     # a
-        # else:
-        #     break
-        # counter += 1
 
 
 if __name__ == '__main__':
